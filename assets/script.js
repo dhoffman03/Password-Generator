f528b8189a242ce16bc79c18d90c1bb7273f52a3
Dymond <dymond.hoffman03@gmail.com>
2022-07-24 22:50:12 -0700

    object-methods and prompts added

--- assets/script.js
+++ assets/script.js
@@ -10,43 +10,72 @@ function writePassword() {
 
 }
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+// var holding password criteria
+var criteria = {
+  //arry holding uppercase letters
+  upperCharacters: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  //arry holding lowercase letters
+  lowerCharacters: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+  //array holding numbers
+  numericCharacters: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  //array holding special characters
+  specialCharacters: ["~","!","#","$","%","&","*","@","?","^","_","-","|","/"],
+}
 
 
 // declare function generatePasswor()
 function generatePassword() {
 
+  // var for selected characters
+  var selectedCharacters = []
+
+
+  //var used for prompts
+  var uppercase;
+  var lowercase;
+  var numeric;
+  var special;
+
+  // Var holding password length prompt 
+  var passwordLength = prompt("How many characters would you like in your password?")
+  // if password length does not meet criteria, stop function
+  if (passwordLength < 8 || passwordLength > 128) {
+    return password;
+  }
+
+  //calls the function to show prompts for criteria
+  criteriaPrompts ();
+
+  return password;
+
+  //internal function to prompt user for password criteria
+  function criteriaPrompts() {
+    uppercase = confirm("Would you like to include uppercase characters?");
+    lowercase = confirm("Would you like to include lowercase characters?");
+    numeric = confirm("Would you like to include numeric characters?");
+    special = confirm("Would you like to include special characters?");
+  }
 }
 
-// var holding differnt characters: i.e. arr[]
-var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowercaseCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialCharacters = ["~","!","#","$","%","&","*","@","?","^","_","-","|","/"];
-// var selectedCharacter = []
-var selectedCharacters = []
-// Use prompt() to gater password criteria
-// var passwordLength = prompt("How many characters would you like in your password?") 
-var passwordLength = prompt("How many characters would you like in your password?")
-// if (paswordLength < 8) stop function || if (paswordLength > 128) stop function
-if (passwordLength < 8 || passwordLength > 128) {
-  return;
-}
-// confirm() for uppercase letters
-// confirm() for lowercase letters
-// confirm() for numeric characters
-// confirm() for special characters
-// one each prompt/confirm is answered 
+
+
+
+// once each prompt/confirm is answered 
 // if confirms are true keep or get character type for the confirm
-// is !confirm stop function
 // if confirm is true then concact related arr to selected characters arr s
 //if(isUppercase) { selectedCharacters.concat(uppercaseCharacters); }
 // i.e. ['A','a', 1, %, $]
+
+
+// is !confirm stop function
+
+
 // for loop on passwordLength 
+
+
 // use Math.floor(Math.random() * selectedCharacters) to get random index of selectedCharacters
-// return password;
-
-
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
